54655baa3cb5ad7eaa5e5b180c86eafb10db5d5d
hourabatool <hourabatool@gmail.com>
2021-08-21 17:01:44 +0500

    day2

---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = 'Houra';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 85.0,
+        title: Text(" Catalog App "),
+      ),
+      body: Material(
+        child: Center(
+          child: Container(
+            child: Text("starting ${days} days of flutter by ${name}"),
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
