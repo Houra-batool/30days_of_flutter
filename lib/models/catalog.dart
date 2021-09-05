00174515ffbbb4625aa8e89ca53af5a9bf312d6a
hourabatool <hourabatool@gmail.com>
2021-09-05 13:01:36 +0500

    day9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,11 @@
+
+class item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({required this.id,required this.name,required this.desc,required this.price,required this.color,required this.image});
+}
